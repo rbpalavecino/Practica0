927cd70d39c23e97028a99637ccc78ee11675d33
reina-r <83967446+reina-r@users.noreply.github.com>
2022-09-30 17:20:57 -0300

    Entrega Práctica 0
    Se realiza entrega según consigna.

--- Practica0/Program.cs
+++ Practica0/Program.cs
@@ -10,6 +10,38 @@ namespace Practica0
     {
         static void Main(string[] args)
         {
+            string infoIngresada = "";
+
+            Console.WriteLine("Ingrese la información:");
+            infoIngresada = Console.ReadLine();
+
+            try
+            {
+                string fecha = infoIngresada.Substring(0, 4) + '/' + infoIngresada.Substring(4, 2) + '/' + infoIngresada.Substring(6, 2);
+                string hora = infoIngresada.Substring(8, 2) + "Hs " + infoIngresada.Substring(10, 2) + "Min " + infoIngresada.Substring(12, 2) + "Seg";
+                string temperatura = infoIngresada.Substring(14, 2) + ',' + infoIngresada.Substring(16, 1);
+                string humedad = infoIngresada.Substring(17, 2) + ',' + infoIngresada.Substring(19, 1);
+
+                string codigoSensor = infoIngresada.Substring(20,4);
+                string estadoActivoSensor = (infoIngresada.Substring(24,1) == "1")? "SI" : "NO";
+
+                //Se elige el primer tipo de respuesta ya que la información queda mejor organizada visualmente para el usuario 
+                Console.WriteLine("Fecha del registro: {0} ", fecha);
+                Console.WriteLine("Hora del registro: {0} ", hora);
+                Console.WriteLine("Temperatura: {0}°", temperatura);
+                Console.WriteLine("Humedad: {0}% ", humedad);
+                Console.WriteLine("Código: “{0}” ", codigoSensor);
+                Console.WriteLine("Activo: {0} ", estadoActivoSensor);
+
+                Console.ReadKey();
+            }
+            catch
+            {
+                Console.WriteLine("Se produjo un error.");
+                Console.ReadKey();
+            }
+
         }
+
     }
 }
